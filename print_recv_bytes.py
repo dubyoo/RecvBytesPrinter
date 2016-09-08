3098b698b2c71db284ded01bf5513ee0435ca738
dylan-work <wangwei1543@foxmail.com>
2016-09-08 15:39:10 +0800

    commit to github

---
+++ print_recv_bytes.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+import sys
+from socket import *
+
+
+MAX_BUFFER_SIZE = 1024
+UDP_IP = "0.0.0.0"
+
+def print_str(str, addr):
+    print "[DEBUG] recv message from %s(%s):" % (addr[0], addr[1])
+    for c in str:
+        if '\r' == c:
+            sys.stdout.write("\\r")
+        elif '\n' == c:
+            sys.stdout.write("\\n")
+        else:
+            sys.stdout.write(c)
+    sys.stdout.write("\n\n")
+    sys.stdout.flush()
+
+
+def print_in_hex(str, addr):
+    print "[DEBUG] recv message(%d bytes) from %s(%s):" % (len(str), addr[0], addr[1])
+    index = 0
+    for c in str:
+        if 0 == index % 4:
+            print "\t%04X " % index,
+        print "%02X" % ord(c),
+        if 3 == index % 4:
+            print
+        index += 1
+    print
+    sys.stdout.flush()
+
+
+# ./print_recv_bytes.py [port]
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print "USAGE :", sys.argv[0], "[Port]"
+        sys.exit()
+
+    UDP_PORT = int(sys.argv[1])
+    sock = socket(AF_INET, SOCK_DGRAM)
+    sock.bind((UDP_IP, UDP_PORT))
+    print "[DEBUG] Linstening port %d ..." % UDP_PORT
+
+    while True:
+        data, addr = sock.recvfrom(MAX_BUFFER_SIZE)
+        print_in_hex(data, addr)
+
+
